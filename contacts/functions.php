a740dcdb20a90f569b86b2ec2c808d1a9cfec034
shelleyvadams <shelleyvadams@gmail.com>
2014-12-16 13:18:12 -0500

    Remove files and code unrelated to individual plugins.

--- contacts/functions.php
+++ contacts/functions.php
@@ -42,38 +42,6 @@
 			'parent' => 'Parent Person',
 		),) );
 
-		register_post_type('services',
-			array(	'label' => 'services',
-			'description' => 'services',
-			'public' => true,
-			'show_ui' => true,
-			'show_in_menu' => true,
-			'show_in_nav_menus' => true,
-			'capability_type' => 'page',
-			'hierarchical' => true,
-			'query_var' => true,
-			'has_archive' => false,
-			'rewrite' => true,
-			'exclude_from_search' => true,
-			'supports' => array('title','page-attributes'),
-			'menu_position' => 28,
-			// 'taxonomies' => array('category'),
-			'labels' => array (
-			'name' => 'Service Times',
-			'singular_name' => 'Service Time',
-			'menu_name' => 'Service Times',
-			'add_new' => 'Add Service Time',
-			'add_new_review' => 'Add New Service Time',
-			'edit' => 'Edit',
-			'edit_review' => 'Edit Service Time',
-			'new_review' => 'New Service Time',
-			'view' => 'View Service Time',
-			'view_review' => 'View Service Time',
-			'search_reviews' => 'Search Service Time',
-			'not_found' => 'No Service Time Found',
-			'not_found_in_trash' => 'No Service Time Found in Trash',
-			'parent' => 'Parent Service Time',
-		),) );
 	}
 
 	/* ========================================================================================================================
@@ -87,5 +55,3 @@
 	include_once 'metaboxes/bio-spec.php';
 
 	include_once 'metaboxes/map-spec.php';
-
-	include_once 'metaboxes/times-spec.php';
--- contacts/sidebar.php
+++ contacts/sidebar.php
@@ -1,77 +1,39 @@
-<aside>
-	<div class="sidebar-contents">
-		<?php
-		$page = get_page_by_title( 'About' );
-		$the_excerpt = $page->post_excerpt;
+<div class="contacts-widget">
+	<h2 class="sidebar">Parish Leaders</h2>
+	<div class="parish-leaders-wrapper">
+
+		<?php $args = array(
+			'post_type'  => 'people',
+			'posts_per_page' => '4',
+			'order'      => 'ASC');
 		?>
 
-		<p><?php echo $the_excerpt ?>&nbsp;&nbsp;<a class="more-link" href="<?php echo get_permalink( get_page_by_path( 'about' ) ) ?>">More...</a></p>
+		<?php
+		global $bio;
+		global $args;
 
-		<h2 class="sidebar">Service Times</h2>
-		<div class="service-times-wrapper">
-			<?php $args = array(
-				'post_type'  => 'services',
-				'posts_per_page' => '-1',
-				'order'      => 'ASC');
+		$the_query = new WP_Query($args);
+
+		if ( $the_query->have_posts() ): ?>
+
+			<?php while ( $the_query->have_posts() ) : $the_query->the_post();
+
+				$number = (trim(get_post_meta($the_query->post->ID, '_ns_number', true)));
+				$email = (trim(get_post_meta($the_query->post->ID, '_ns_email', true)));
 			?>
+				<article class="people-sidebar clearfix">
+					<?php the_post_thumbnail(); ?>
+					<ul>
+						<li><h4 class="people"><?php the_title(); ?></h4></li>
+						<li><?php echo $number ?></li>
+						<li><a href="mailto:<?php echo $email ?>"><?php echo $email ?></a></li>
+					</ul>
+				</article>
 
-			<?php
-			global $services;
-			global $args;
+			<?php endwhile; ?>
+		<?php endif; ?>
 
-			$the_query = new WP_Query($args);
-
-			if ( $the_query->have_posts() ): ?>
-
-				<?php while ( $the_query->have_posts() ) : $the_query->the_post();
-
-					$times = (trim(get_post_meta($the_query->post->ID, '_ns_times', true)));
-				?>
-					<article class="clearfix">
-						<h5><?php the_title(); ?> - <span class="times"><?php echo $times ?></span></h5>
-					</article>
-
-				<?php endwhile; ?>
-			<?php endif; ?>
-		</div>
-
-		<h2 class="sidebar">Parish Leaders</h2>
-		<div class="parish-leaders-wrapper">
-
-			<?php $args = array(
-				'post_type'  => 'people',
-				'posts_per_page' => '4',
-				'order'      => 'ASC');
-			?>
-
-			<?php
-			global $bio;
-			global $args;
-
-			$the_query = new WP_Query($args);
-
-			if ( $the_query->have_posts() ): ?>
-
-				<?php while ( $the_query->have_posts() ) : $the_query->the_post();
-
-					$number = (trim(get_post_meta($the_query->post->ID, '_ns_number', true)));
-					$email = (trim(get_post_meta($the_query->post->ID, '_ns_email', true)));
-				?>
-					<article class="people-sidebar clearfix">
-						<?php the_post_thumbnail(); ?>
-						<ul>
-							<li><h4 class="people"><?php the_title(); ?></h4></li>
-							<li><?php echo $number ?></li>
-							<li><a href="mailto:<?php echo $email ?>"><?php echo $email ?></a></li>
-						</ul>
-					</article>
-
-				<?php endwhile; ?>
-			<?php endif; ?>
-
-			<p class="button"><a href="<?php echo get_permalink( get_page_by_path( 'contacts' ) ) ?>">View Full List</a>
-		</div>
-		<!--/.parish-leaders-wrapper-->
+		<p class="button"><a href="<?php echo get_permalink( get_page_by_path( 'contacts' ) ) ?>">View Full List</a>
 	</div>
-	<!--/.sidebar-contents-->
-</aside>
+	<!--/.parish-leaders-wrapper-->
+</div>
