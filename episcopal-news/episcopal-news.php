68008ea4e9ab43d864139ad875cb8ec3360d4460
shelleyvadams <shelleyvadams@gmail.com>
2014-12-22 15:18:46 -0500

    episcopal-news: two working shortcodes. Yippee.

--- episcopal-news/episcopal-news.php
+++ episcopal-news/episcopal-news.php
@@ -1,23 +1,76 @@
-<?php /* Template name: Home */ ?>
+<?php
+/*
+ * Plugin Name:  Episcopal News
+ * Plugin URI:   https://github.com/shelleyvadams/episcopal_wordpress-plugins
+ * Description:  Headlines from Episcopal News Service and featured videos from The Episcopal Church, Office of Communication.
+ * Version:      0.0.1
+ * Author:       Shelley V. Adams
+ * Author URI:   http://shelleyvadams.github.io/
+ * License:      GPLv2+
+*/
 
-<section>
-	<h4>Around The Episcopal Church</h4>
-	<article class="video-post">
-		<iframe src="//fast.wistia.net/embed/playlists/5nzsiujrhe?media_0_0%5BautoPlay%5D=false&media_0_0%5BcontrolsVisibleOnLoad%5D=false&theme=bento&version=v1&videoOptions%5BautoPlay%5D=true&videoOptions%5BsmallPlayButton%5D=false&videoOptions%5BvideoHeight%5D=225&videoOptions%5BvideoWidth%5D=400&videoOptions%5BvolumeControl%5D=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_playlist" name="wistia_playlist" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="100%" height="325"></iframe>
-	</article>
+// Define URLs as constants for easy updating.
 
-	<?php
+define('EPISCOPAL_NEWS_FEED',  'http://episcopaldigitalnetwork.com/ens/feed/');
+define('EPISCOPAL_NEWS_VIDEO', '//fast.wistia.net/embed/playlists/5nzsiujrhe?media_0_0%5BautoPlay%5D=false&media_0_0%5BcontrolsVisibleOnLoad%5D=false&theme=bento&version=v1&videoOptions%5BautoPlay%5D=true&videoOptions%5BsmallPlayButton%5D=false&videoOptions%5BvideoHeight%5D=225&videoOptions%5BvideoWidth%5D=400&videoOptions%5BvolumeControl%5D=true');
+
+
+
+/**
+ * Fetch and display headlines from Episcopal News Service
+ *
+ * @todo Add parameter for the number of items displayed
+ * @todo Add boolean parameter for displaying blurbs with headlines
+ */
+
+function en_news_display( $atts, $content = null ) {
+
 	include_once(ABSPATH.WPINC.'/rss.php'); // path to include script
-	$feed = fetch_rss('http://episcopaldigitalnetwork.com/ens/feed/'); // specify feed url
+	$feed = fetch_rss(EPISCOPAL_NEWS_FEED); // specify feed url
 	$items = array_slice($feed->items, 0, 2); // specify first and last item
-	?>
 
-	<?php if (!empty($items)) : ?>
-	<?php foreach ($items as $item) : ?>
-		<article class="rss-post">
-			<h2><a href="<?php echo $item['link']; ?>" target="_blank"><?php echo $item['title']; ?> </a></h2>
-			<p><?php echo $item['description']; ?></p>
-		</article>
-	<?php endforeach; ?>
-	<?php endif; ?>
-</section>
+	$newsoutput = '';
+
+ if (!empty($items)) {
+		ob_start(); // for shortcodes, use output buffer instead of echo
+	?>
+		<ul class="episcopal-news feed episcopal-news-feed">
+			<?php foreach ($items as $item) : ?>
+				<li class="episcopal-news feed-item episcopal-news-feed-item">
+					<a href="<?php echo $item['link']; ?>" target="_blank"><?php echo $item['title']; ?></a>
+					<p><?php echo $item['description']; ?></p>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	<?php
+		$newsoutput .= ob_get_contents();
+		ob_end_clean();
+	} //if
+
+	return $newsoutput;
+}
+
+
+/**
+ * Embed featured videos from The Episcopal Church, Office of Communication
+ */
+
+function en_video_display( $atts, $content = null ) {
+	$vidoutput = '<div class="episcopal-news video episcopal-news-video"><iframe class="wistia_playlist" src="';
+	$vidoutput .= EPISCOPAL_NEWS_VIDEO;
+	$vidoutput .= '" allowfullscreen="true" width="665" height="225"></iframe></div>';
+	return $vidoutput;
+}
+
+/**
+ * Register the short codes.
+ */
+
+function en_register_shortcodes() {
+	add_shortcode( 'episcopalnews', 'en_news_display' );
+	add_shortcode( 'episcopalvideo', 'en_video_display' );
+}
+
+// Finally, add stuff to WordPress.
+
+add_action( 'init', 'en_register_shortcodes' );
--- episcopal-news/readme.txt
+++ episcopal-news/readme.txt
@@ -1,42 +1,85 @@
 === Episcopal News ===
-Contributors: (this should be a list of wordpress.org userid's)
+Contributors: shelleyvadams
 Donate link: http://example.com/
 Tags: Episcopal Church, news, headlines, video, embed
 Requires at least: 4.0.1
-Tested up to: 4.01
+Tested up to: 4.0.1
 Stable tag: 1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 Headlines from Episcopal News Service and featured videos from The Episcopal Church, Office of Communication.
 
+
 == Description ==
 
 Plugin features:
 
-* Widget and shortcode to display Episcopal News Service headlines.
+* Shortcode to display Episcopal News Service headlines (Widget coming soon).
 * Shortcode to embed The Episcopal Church, Office of Communication featured videos playlist.
+
+This plugin was initially derived from content that was hard-coded into a
+front page template of themes developed for The Episcopal Church and offered as
+a free website resource for dioceses and congregations.
+
 
 == Installation ==
 
-Do **not** install this plugin yet. It doesn't work yet.
+After activating the plugin, you can add headlines to your WordPress site using
+the shortcode `[episcopalnews]`. In future releases, this shortcode will support
+parameters to control the number of headlines as well as the display of item
+summaries and thumbnail images.
+
+The featured videos playlist can be embedded with the shortcode
+`[episcopalvideo]`. Currently, there are no plans to add parameters to this
+shortcode.
+
 
 == Frequently Asked Questions ==
 
-= A question that someone might have =
+= What is Episcopal News Service? =
 
-An answer to that question.
+From &ldquo;[About Episcopal News Service](http://episcopaldigitalnetwork.com/ens/about-episcopal-news-service/)&rdquo;:
 
-= What about foo bar? =
+> Episcopal News Service (ENS) offers in-depth reporting and analysis of local, regional, national and international news for Episcopalians and others interested in the church’s mission and ministry. Providing written and multimedia coverage, Episcopal News Service is the officially sponsored online news source of the Episcopal Church.
 
-Answer to foo bar dilemma.
+[That page](http://episcopaldigitalnetwork.com/ens/about-episcopal-news-service/)
+also provides information about copying and republishing content.
+
+= How can I submit content to Episcopal News Service? =
+
+[Submission guidelines](http://episcopaldigitalnetwork.com/ens/submission-guidelines/)
+are listed on the Episcopal News Service website.
+
+= How is the video embedded? =
+
+As of version 0.0.1, video is embedded in an `iframe` element. At the time of
+writing (December 2014), video is streamed through a Flash object inside that
+frame.
+
+= What is the video source? =
+
+As of version 0.0.1, [Wistia](http://wistia.com/) is the video source. At the
+time of writing (December 2014), Content was served from the `wistia.com` and
+`wistia.net` domains.
+
+The Episcopal Church also has videos on
+[YouTube](https://www.youtube.com/user/EpiscopalChurchYT) and
+[Vimeo](http://vimeo.com/episcopalchurch). Several plugins are availible to
+embed video from these sources.
+
+= Can you add my (church's) video to the playlist? =
+
+No. Contact the [Office of Communication](http://www.episcopalchurch.org/office/office-communication).
+
 
 == Changelog ==
 
 = 0.0.1 =
-* Non-release. Just committing the readme.txt to GitHub
+* First alpha release.
+
 
 == Upgrade Notice ==
 
 = 0.0.1 =
-* Nothing to upgrade from.
+* First alpha release, there's nothing to upgrade from.
